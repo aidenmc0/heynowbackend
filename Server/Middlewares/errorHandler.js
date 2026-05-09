ead5c38781fa06f50c0ce7e58aa2fdfcae7e731f
aidenmc0 <aidenmccourtt@gmail.com>
2026-05-09 11:32:38 +0000

    Del

--- Server/Middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error('❌ Error:', err.message);
-  res.status(err.status || 500).json({ 
-    message: err.message || 'Server Error' 
-  });
-};
